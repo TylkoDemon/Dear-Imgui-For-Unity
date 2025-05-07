1a73e887627ed871789b828263373f7614bfd455
Adam Majcherek <Dem0n1358v2@gmail.com>
2025-05-07 16:50:18 +0200

    Some project fixes after IMGUI and Unity version updates.

--- Assets/HelloDearImgui.cs
+++ Assets/HelloDearImgui.cs
@@ -6,16 +6,24 @@ public sealed class HelloDearImgui : MonoBehaviour
 {
     private void OnEnable()
     {
+        DearImGui.AddDrawRequest(OnShouldRenderer);
         ImGuiUn.Layout += OnImGUI;
     }
     
     private void OnDisable()
     {
+        DearImGui.RemoveDrawRequest(OnShouldRenderer);
         ImGuiUn.Layout -= OnImGUI;
+    }
+    
+    private bool OnShouldRenderer()
+    {
+        return enabled && gameObject.activeInHierarchy;
     }
     
     private void OnImGUI()
     {
+        Debug.Log("Hello Dear ImGui");
         ImGui.ShowDemoWindow();
     }
 }
